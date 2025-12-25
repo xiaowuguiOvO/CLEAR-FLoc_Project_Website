c8b2dc1192f3c49768558e6958bb43f29e3ab40e
Shiyong Meng <129972764+xiaowuguiOvO@users.noreply.github.com>
2025-12-25 17:06:21 +0800

    can run

--- static/js/index.js
+++ static/js/index.js
@@ -1,78 +1,64 @@
-window.HELP_IMPROVE_VIDEOJS = false;
-
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
-
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
-
-
 $(document).ready(function() {
     // Check for click events on the navbar burger icon
     $(".navbar-burger").click(function() {
       // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
       $(".navbar-burger").toggleClass("is-active");
       $(".navbar-menu").toggleClass("is-active");
-
     });
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
+    // --- Visualization Selector Logic ---
+    var sceneList = [
+        "3250_17.png",
+        "3250_5.png",
+        "3251_24.png",
+        "3254_15.png",
+        "3266_1.png",
+        "3269_21.png",
+        "3281_8.png",
+        "3292_21.png",
+        "3297_12.png"
+    ];
+
+    var basePaths = {
+        "semray": "./static/images/semray/",
+        "unlocs": "./static/images/unlocs/",
+        "disamb": "./static/images/disamb/",
+        "ours": "./static/images/ours/"
+    };
+
+    function updateComparison(filename) {
+        $("#vis-semray").attr("src", basePaths["semray"] + filename);
+        $("#vis-unlocs").attr("src", basePaths["unlocs"] + filename);
+        $("#vis-disamb").attr("src", basePaths["disamb"] + filename);
+        $("#vis-ours").attr("src", basePaths["ours"] + filename);
+        
+        // Highlight selected thumbnail
+        $(".vis-thumb").removeClass("is-active-thumb");
+        $(`.vis-thumb[data-file='${filename}']`).addClass("is-active-thumb");
     }
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
+    // Initialize thumbnails
+    var $thumbContainer = $("#vis-thumbnails");
+    sceneList.forEach(function(filename, index) {
+        var $col = $("<div>").addClass("column is-4-mobile is-3-tablet"); // Even larger: 3 per row on mobile, 4 per row on desktop
+        var $img = $("<img>")
+            .attr("src", "./static/images/imgs/" + filename)
+            .addClass("vis-thumb image")
+            .css("cursor", "pointer")
+            .attr("data-file", filename)
+            .click(function() {
+                updateComparison(filename);
+            });
+        
+        if (index === 0) $img.addClass("is-active-thumb"); // Select first by default
+        
+        $col.append($img);
+        $thumbContainer.append($col);
     });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
-    bulmaSlider.attach();
-
+    // Initial load
+    if (sceneList.length > 0) {
+        updateComparison(sceneList[0]);
+    }
+    // ------------------------------------
 })
